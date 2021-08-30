9f2b2734e3f44f5b78f2e3221672ab8d42085632
Minn.Wu <45867985+MinnWu@users.noreply.github.com>
2021-08-30 18:50:16 +0800

    Add files via upload

---
+++ addProduct.js
@@ -0,0 +1,162 @@
+var list = ['名称', '价值结构', '合约', '起始日期', '初始价格', '结束日期', '末期价格', '波动幅度', '波动率', '完成进度', '发现价值交易机会结构', '单位溢价金额', '溢价率', '隐含波动率']
+var _product = {
+    'pName': '',     //商品名称
+    'VC': '',   //价值结构
+    'contract': '',      //合约
+    'startDate': '',     //起始日期
+    'startPrice': '',     //初始价格
+    'endDate': '',       //结束日期
+    'endPrice': '',  //末期价格
+    'FR': '',  //波动幅度
+    'volatility': '',    //波动率
+    'schedule': '',      //进度
+    'DVTOS': '',     //发现价值交易机会结构
+    'UPA': '',   //单位溢价金额
+    'PR': '',    //溢价率
+    'IV': ''     //隐含波动率
+}
+
+//初始化生成10行商品信息列表
+const initProduct = (num = 10) => {
+    for (let index = 0; index < num; index++) {
+        productList(_product)
+    }
+}
+
+//生成商品信息列表
+const productList = (obj, divele = 'addlist') => {
+    let div, input, arr, add, del, eleObj
+
+    eleObj = document.getElementById(divele)
+    div = document.createElement('div')
+    div.className = 'row'
+    arr = Object.keys(obj)
+    for (let index = 0; index < list.length; index++) {
+        input = document.createElement('input')
+        input.type = 'text'
+        input.placeholder = list[index]
+        input.id = arr[index]
+        div.appendChild(input)
+    }
+    add = document.createElement('input')
+    add.className = 'button'
+    add.type = 'button'
+    add.id = 'add'
+    add.value = '增加'
+    div.appendChild(add)
+    del = document.createElement('input')
+    del.className = 'button'
+    del.type = 'button'
+    del.id = 'del'
+    del.value = '删除'
+    div.appendChild(del)
+    eleObj.appendChild(div)
+}
+
+
+//增加商品信息列表
+const addElement = (obj, ele) => {
+    let div, input, arr, add, del, targetElement, parent
+    targetElement = document.getElementById('add').parentNode
+    parent = targetElement.parentNode
+    div = document.createElement('div')
+    div.className = 'row'
+    arr = Object.keys(obj)
+    for (let index = 0; index < list.length; index++) {
+        input = document.createElement('input')
+        input.type = 'text'
+        input.placeholder = list[index]
+        input.id = arr[index]
+        div.appendChild(input)
+    }
+    add = document.createElement('input')
+    add.className = 'button'
+    add.type = 'button'
+    add.id = 'add'
+    add.value = '增加'
+    div.appendChild(add)
+    del = document.createElement('input')
+    del.className = 'button'
+    del.type = 'button'
+    del.id = 'del'
+    del.value = '删除'
+    div.appendChild(del)
+    ele.after(div)
+}
+
+//删除商品信息列表
+const delProduct = (ele) => {
+    if (document.querySelectorAll('#del').length != 1) {
+        ele.remove()
+    } else {
+        console.log('至少要保留一个商品，不可以删除完。')
+    }
+}
+
+//获取单行商品信息列表的值，返回一个json对象
+const getValue = (index) => {
+    var product = JSON.parse(JSON.stringify(_product));
+    var array = document.querySelectorAll('#addlist div');
+    product.pName = array[index].querySelector('#pName').value;
+    product.VC = array[index].querySelector('#VC').value;
+    product.contract = array[index].querySelector('#contract').value;
+    product.startDate = array[index].querySelector('#startDate').value;
+    product.startPrice = array[index].querySelector('#startPrice').value;
+    product.endDate = array[index].querySelector('#endDate').value;
+    product.endPrice = array[index].querySelector('#endPrice').value;
+    product.FR = array[index].querySelector('#FR').value;
+    product.volatility = array[index].querySelector('#volatility').value;
+    product.schedule = array[index].querySelector('#schedule').value;
+    product.DVTOS = array[index].querySelector('#DVTOS').value;
+    product.UPA = array[index].querySelector('#UPA').value;
+    product.PR = array[index].querySelector('#PR').value;
+    product.IV = array[index].querySelector('#IV').value;
+    return product;
+}
+
+//获取所有商品列表信息行的值，返回一个json对象
+const getData = () => {
+    var obj = {}
+    var array = document.querySelectorAll('#addlist div')
+    for (let index = 0; index < array.length; index++) {
+        if (array[index].querySelector('#pName').value != "") {
+            var pName = array[index].querySelector('#pName').value
+            obj[pName] = getValue(index)
+        } else { return obj }
+    }
+    return obj;
+}
+
+$(function () {
+    // initProduct();
+    $('div #show').on('click', function () {
+        $('#productlist').hide();
+        $('#addlist').empty()//删除addlist下的所有元素，也就是清空列表
+    });
+    $('div #add').on('click', function () {
+        if ($('.row').length > 0) {
+            $('#productlist').show()
+        } else {
+            initProduct();
+            $('#productlist').show()
+        }
+    });
+    $('#addlist').delegate('#add', 'click', function () {
+        // $(this).parent() 此#add元素的父级元素
+        addElement(_product, $(this).parent())
+    });
+    $('#addlist').delegate('#del', 'click', function () {
+        delProduct($(this).parent())
+    });
+    $('#submit').on('click', function () {
+        var db = JSON.stringify(getData())
+        if (db == "{}") {
+        } else {
+            $.post("/postdata", { data: db }, function (result) {
+                console.log(result)
+                $('.prompt').show()
+                document.querySelector('.prompt span').innerHTML = result
+            });
+        }
+    });
+});
---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const fs = require('fs')
+const dataPath = './data.json'
+
+//读取数据
+const readData = (path = dataPath) => {
+    try {
+        return JSON.parse(fs.readFileSync(path, 'utf8'))
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
+
+//写入数据
+const writeData = (data, path = dataPath) => {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const Result = (path = dataPath) => {
+    //拿到data.json的数据给obj对象
+    var obj = JSON.parse(fs.readFileSync(path, 'utf8'))
+    //用一个数据arr来接收obj对象的所有子对象，每个子对象表示一条记录，相当于obj有多少条记录
+    var arr = Object.keys(obj)
+
+    var val = []
+    //遍历每条记录，调用calculate函数的规则对其进行判断买还是卖
+    for (let index = 0; index < arr.length; index++) {
+        if (calculate(obj[arr[index]]) > 10) {
+            val.push(arr[index] + ': 买')
+        } else {
+            val.push(arr[index] + ': 卖')
+        }
+    }
+    return val
+}
+
+//计算规则
+const calculate = (obj) => {
+    return Number(obj.startPrice) + Number(obj.endPrice)
+}
+
+app.use('/', express.static('./'));
+
+app.get('/addProduct.html', function (req, res) {
+    res.sendFile(__dirname + "/" + "addProduct.html");
+})
+
+//处理get请求，响应data.json的数据
+app.get("/getdata", (req, res) => {
+    res.send(readData())
+});
+
+//处理post请求，响应200
+app.post('/postdata', (req, res) => {
+    writeData(JSON.parse(req.body.data))
+    // res.send(JSON.stringify(res.statusCode))
+    res.send(Result())
+})
+
+//创建服务并监听3000端口
+app.listen(3000)
+console.log('服务已启动...')
+
